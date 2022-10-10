c0acadbe004695ea78bae85aec3c373ad6283323
Priyanshu Jha <95862487+CoderPriyanshuJha@users.noreply.github.com>
2022-10-10 11:47:29 +0530

    Traversal of an Array
    C program to traverse an array.

---
+++ arrays/array_traversal.c
@@ -0,0 +1,15 @@
+/* C program to traverse an array */
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {4, 5, 6, 7, 9};
+    // traversal
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i];
+    }
+
+    return 0;
+}
